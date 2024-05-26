50d986a403212f6d5df7a0ef7f0cf57970f43dbc
noor-nazim <nazim35-709@diu.edu.bd>
2024-05-26 23:01:49 +0600

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        if(Auth::user()->usertype == 'user'){
+
+            return view('dashboard');
+        }
+
+        else{
+
+            return view('admin.home');
+        }
+    }
+}
